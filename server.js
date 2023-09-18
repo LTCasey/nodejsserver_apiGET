62d673730dfc17f4b63d6d019496f259e0d15dc8
LTCasey <139070574+LTCasey@users.noreply.github.com>
2023-09-18 18:04:03 -0500

    Create server.js
    In this code: 
    Replace const apiEndpoint = 'https://your-api-endpoint-here'; with the actual URL or endpoint you want to fetch data from.
    
    This is the server side JS server. Not in public_html folder but points to the client side stuff that would be.

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public_html'));
+app.use(cors());
+
+// Define your API endpoint here
+const apiEndpoint = 'https://your-api-endpoint-here';
+
+app.get('/getData', async (req, res) => {
+  try {
+    const response = await axios.get(apiEndpoint);
+    const { followers, subscribers } = response.data;
+
+    // Extract the required data
+    const dataToSend = {
+      numFollowers: followers.num_followers,
+      latestFollowerUsername: followers.latest_follower.username,
+      numSubscribers: subscribers.num_subscribers,
+      latestSubscriberUsername: subscribers.latest_subscriber.user,
+    };
+
+    res.json(dataToSend);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while fetching the data' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
